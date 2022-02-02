911ac75623fb0a50bd1ce4b19673d93faf57dc47
TheBoringBot <richardsgareth@icloud.com>
2022-02-02 22:20:55 +0100

    Updated sound migration with new fields

--- database/migrations/2022_01_27_185521_create_sounds_table.php
+++ database/migrations/2022_01_27_185521_create_sounds_table.php
@@ -4,37 +4,41 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSoundsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-       Schema::create( 'sounds', function ( Blueprint $table ) {
+class CreateSoundsTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create( 'sounds', function ( Blueprint $table ) {
 			$table->id();
 			$table->unsignedBigInteger( 'user_id' );
 			$table->string( 'name' );
 			$table->text( 'description' );
-			$table->time( 'duration' );
-			$table->smallInteger( 'bit_depth' );
-			$table->smallInteger( 'bit_rate' );
+			$table->string( 'slug' );
+			$table->time( 'duration_seconds' );
+			$table->string( 'duration_string' );
+			$table->string( 'file_size' );
+			$table->tinyInteger( 'bit_depth' );
+			$table->integer( 'bit_rate' );
+			$table->integer( 'sample_rate' );
 			$table->string( 'file_url' );
 			$table->integer( 'likes' );
 			$table->integer( 'plays' );
 			$table->timestamps();
-		} );
-    }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-       	Schema::dropIfExists( 'sounds' );
-    }
+
+
+		} );
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists( 'sounds' );
+	}
 }
