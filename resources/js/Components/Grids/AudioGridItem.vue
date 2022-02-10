5d4581ac9cda070d68b5be123272ed5f151550a3
TheBoringBot <richardsgareth@icloud.com>
2022-02-10 11:58:23 +0100

    added clipboard copy + toasters

--- resources/js/Components/Grids/AudioGridItem.vue
+++ resources/js/Components/Grids/AudioGridItem.vue
@@ -1,5 +1,80 @@
-<template><div class="py-28 bg-white">1</div></template>
+<template>
+    <div class="bg-slate-200 rounded relative flex-col">
+        <!-- Waveform -->
+        <div class="relative h-32"></div>
+        <!-- buttons -->
+        <div class="w-full p-4 bg-white">
+            <div class="absolute top-0 right-0 p-3">
+                <!-- Info -->
+                <button
+                    v-tooltip="'Show Info'"
+                    class="bg-purple-600 hover:bg-purple-700 p-1 px-3 text-white font-semibold text-xs flex justify-center items-center rounded-full"
+                >
+                    Info
+                </button>
+            </div>
+
+            <div
+                class="flex align-middle text-purple-600 text-lg justify-between"
+            >
+                <!-- Play -->
+                <button
+                    class="text-5xl flex justify-center items-center rounded-full hover:text-purple-700"
+                >
+                    <font-awesome-icon icon="circle-play" />
+                </button>
+                <!-- Pause -->
+                <button class="text-5xl hidden hover:text-purple-700">
+                    <font-awesome-icon icon="circle-pause" />
+                </button>
+                <div class="bg-purple-50 py-2 px-6 rounded-full w-50">
+                    <!-- Like -->
+                    <button v-tooltip="'Like'" class="hover:text-purple-700">
+                        <font-awesome-icon icon="thumbs-up" />
+                    </button>
+                    <!-- Share -->
+                    <button
+                        @click="doCopy('ADD SOUND URL HERE TO BE COPIED')"
+                        class="ml-4 hover:text-purple-700"
+                        v-tooltip="'Copy Link'"
+                        :showTriggers="(triggers) => [...triggers, 'click']"
+                    >
+                        <font-awesome-icon icon="link" />
+                    </button>
+                    <!-- Download -->
+                    <button
+                        v-tooltip="'Download'"
+                        class="ml-4 hover:text-purple-700"
+                    >
+                        <font-awesome-icon icon="arrow-down" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
 
 <script>
-export default {};
+import { copyText } from "vue3-clipboard";
+import { useToast } from "vue-toastification";
+
+export default {
+    setup() {
+        // Get toast interface
+        const toast = useToast();
+
+        const doCopy = (Url) => {
+            copyText(Url, undefined, (error, event) => {
+                if (error) {
+                    toast(`error -  ${error}`);
+                } else {
+                    // Show toast
+                    toast("Copied Url");
+                }
+            });
+        };
+
+        return { doCopy, toast };
+    },
+};
 </script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,53 @@ require("./bootstrap");
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
+import FloatingVue from "floating-vue";
 import AudioVisual from "vue-audio-visual";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import "floating-vue/dist/style.css";
+import VueClipboard from "vue3-clipboard";
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
+import {
+    faUserSecret,
+    faCirclePause,
+    faCirclePlay,
+    faArrowDown,
+    faLink,
+    faThumbsUp,
+    faInfo,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// Options for Toast Notifications
+const toastOptions = {
+    transition: "Vue-Toastification__fade",
+    maxToasts: 5,
+    newestOnTop: true,
+    position: "top-right",
+    timeout: 1990,
+    closeOnClick: true,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    draggable: true,
+    draggablePercent: 0.3,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: false,
+    icon: true,
+    rtl: false,
+};
+
+library.add(
+    faUserSecret,
+    faCirclePlay,
+    faCirclePause,
+    faArrowDown,
+    faLink,
+    faThumbsUp,
+    faInfo
+);
 
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
@@ -16,7 +62,13 @@ createInertiaApp({
             .use(plugin)
             .mixin({ methods: { route } })
             .use(AudioVisual)
-
+            .component("font-awesome-icon", FontAwesomeIcon)
+            .use(FloatingVue)
+            .use(VueClipboard, {
+                autoSetContainer: true,
+                appendToBody: true,
+            })
+            .use(Toast, toastOptions)
             .mount(el);
     },
 });
