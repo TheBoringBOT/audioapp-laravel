a99249db6d1b5de1a8e6238f7f4c438330664245
TheBoringBot <richardsgareth@icloud.com>
2022-04-23 22:40:31 +0200

    refactor: commented out register form for demo

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,76 +1,83 @@
 <template>
-    <Head title="Register"/>
+    <Head title="Register" />
 
-    <BreezeValidationErrors class="mb-4"/>
+    <BreezeValidationErrors class="mb-4 relative" />
+    <div class="flex items-center py-2 justify-center bg-blue-500 rounded">
+        <span class="mx-auto my-2 text-white text-center"
+            >Registration removed for this demo</span
+        >
+    </div>
 
-    <form @submit.prevent="submit">
-        <div>
-            <BreezeLabel for="name" value="Name"/>
-            <BreezeInput
-                    id="name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.name"
-                    required
-                    autofocus
-                    autocomplete="name"
-            />
-        </div>
+    <!--Registration form removed for demo but works so just un-comment-->
+    <!--<form @submit.prevent="submit">-->
+    <!--<div>-->
+    <!--<BreezeLabel for="name" value="Name" />-->
+    <!--<BreezeInput-->
+    <!--id="name"-->
+    <!--type="text"-->
+    <!--class="mt-1 block w-full"-->
+    <!--v-model="form.name"-->
+    <!--required-->
+    <!--autofocus-->
+    <!--autocomplete="name"-->
+    <!--/>-->
+    <!--</div>-->
 
-        <div class="mt-4">
-            <BreezeLabel for="email" value="Email"/>
-            <BreezeInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autocomplete="username"
-            />
-        </div>
+    <!--<div class="mt-4">-->
+    <!--<BreezeLabel for="email" value="Email" />-->
+    <!--<BreezeInput-->
+    <!--id="email"-->
+    <!--type="email"-->
+    <!--class="mt-1 block w-full"-->
+    <!--v-model="form.email"-->
+    <!--required-->
+    <!--autocomplete="username"-->
+    <!--/>-->
+    <!--</div>-->
 
-        <div class="mt-4">
-            <BreezeLabel for="password" value="Password"/>
-            <BreezeInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="new-password"
-            />
-        </div>
-        <div class="mt-4">
-            <BreezeLabel for="password_confirmation" value="Confirm Password"/>
-            <BreezeInput
-                    id="password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password_confirmation"
-                    required
-                    autocomplete="new-password"
-            />
-        </div>
+    <!--<div class="mt-4">-->
+    <!--<BreezeLabel for="password" value="Password" />-->
+    <!--<BreezeInput-->
+    <!--id="password"-->
+    <!--type="password"-->
+    <!--class="mt-1 block w-full"-->
+    <!--v-model="form.password"-->
+    <!--required-->
+    <!--autocomplete="new-password"-->
+    <!--/>-->
+    <!--</div>-->
+    <!--<div class="mt-4">-->
+    <!--<BreezeLabel for="password_confirmation" value="Confirm Password" />-->
+    <!--<BreezeInput-->
+    <!--id="password_confirmation"-->
+    <!--type="password"-->
+    <!--class="mt-1 block w-full"-->
+    <!--v-model="form.password_confirmation"-->
+    <!--required-->
+    <!--autocomplete="new-password"-->
+    <!--/>-->
+    <!--</div>-->
 
-        <div class="flex-col">
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                        :href="route('login')"
-                        class="underline text-sm text-gray-600 hover:text-gray-900"
-                >
-                    Already have an account?
-                </Link>
+    <!--<div class="flex-col">-->
+    <!--<div class="flex items-center justify-end mt-4">-->
+    <!--<Link-->
+    <!--:href="route('login')"-->
+    <!--class="underline text-sm text-gray-600 hover:text-gray-900"-->
+    <!--&gt;-->
+    <!--Already have an account?-->
+    <!--</Link>-->
 
-                <BreezeButton
-                        class="ml-4"
-                        :class="{ 'opacity-25': form.processing }"
-                        :disabled="form.processing"
-                >
-                    Create Account
-                </BreezeButton>
-            </div>
-        </div>
-    </form>
+    <!--<BreezeButton-->
+    <!--@click="() => this.toast.info('Not working in demo app')"-->
+    <!--class="ml-4"-->
+    <!--:class="{ 'opacity-25': form.processing }"-->
+    <!--:disabled="form.processing"-->
+    <!--&gt;-->
+    <!--Create Account-->
+    <!--</BreezeButton>-->
+    <!--</div>-->
+    <!--</div>-->
+    <!--</form>-->
 </template>
 
 <script>
@@ -79,7 +86,8 @@ import BreezeGuestLayout from "@/Components/Layouts/Guest.vue";
 import BreezeInput from "@/Components/Input.vue";
 import BreezeLabel from "@/Components/Label.vue";
 import BreezeValidationErrors from "@/Components/ValidationErrors.vue";
-import {Head, Link} from "@inertiajs/inertia-vue3";
+import { useToast } from "vue-toastification";
+import { Head, Link } from "@inertiajs/inertia-vue3";
 
 export default {
     layout: BreezeGuestLayout,
@@ -107,11 +115,18 @@ export default {
 
     methods: {
         submit() {
-            this.form.post(this.route("register"), {
-                onFinish: () =>
-                    this.form.reset("password", "password_confirmation"),
-            });
+            // removed for live demo
+            // this.form.post(this.route("register"), {
+            //     onFinish: () =>
+            //         this.form.reset("password", "password_confirmation"),
+            // });
         },
+    },
+
+    setup() {
+        // create toast notifications function
+        const toast = useToast();
+        return { toast };
     },
 };
 </script>
