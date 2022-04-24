baaeb7c60edf9e87ab8f7b0a7257487a69e8104a
TheBoringBot <richardsgareth@icloud.com>
2022-04-24 12:31:42 +0200

    style: new color for navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
                 "primary-bg": "#101010",
                 "primary-bg-hover": "#18181c",
                 "secondary-bg": "rgba(255,255,255,0.05)",
+                "nav-bg": "#1c1c1c",
                 "secondary-bg-hover": "rgba(255,255,255,0.08)",
                 "brand-clr": "#ffca00",
                 "brand-clr-hover": "#ffd740",
