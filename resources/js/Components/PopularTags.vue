b57daa38911c82a261ab1a6c2c41e7772ba10515
TheBoringBot <richardsgareth@icloud.com>
2022-04-24 12:30:54 +0200

    refactor: changed sound file validation to acccept only mpeg/wave refactor: toString() added to make sure tag is string

--- resources/js/Components/PopularTags.vue
+++ resources/js/Components/PopularTags.vue
@@ -5,15 +5,17 @@
         >
         <div class="flex-row">
             <Link
+                v-for="tag in popularTags"
+                :key="tag.id"
                 :href="`/search` + '?' + `keyword=${tag}`"
                 :class="
-                    currentTag && currentTag.toLowerCase() === tag.toLowerCase()
+                    currentTag &&
+                    currentTag.toString().toLowerCase() ===
+                        tag.toString().toLowerCase()
                         ? 'bg-secondary-bg-hover text-brand-clr'
                         : 'text-primary-clr'
                 "
                 class="cursor-pointer font-semibold bg-secondary-bg hover:bg-secondary-bg-hover transition duration-150 ease-in-out px-3 py-2 mr-3 rounded"
-                v-for="tag in popularTags"
-                :key="tag.id"
                 ><span class="capitalize inline-block">{{ tag }}</span></Link
             >
         </div>
