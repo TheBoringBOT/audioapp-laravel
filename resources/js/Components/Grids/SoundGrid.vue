3bb5e94db6a5dd07f0a9e57f8fc9a62115d520c7
TheBoringBot <richardsgareth@icloud.com>
2022-05-05 20:00:23 +0200

    feat: stagger animation on load for sound items

--- resources/js/Components/Grids/SoundGrid.vue
+++ resources/js/Components/Grids/SoundGrid.vue
@@ -1,16 +1,23 @@
 <template>
-    <div class="py-3 grid gap-5 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+    <StaggeredFade
+        :duration="100"
+        tag="div"
+        class="py-3 grid gap-5 grid-cols-1 md:grid-cols-2 xl:grid-cols-3"
+    >
         <AudioItem
+            class="transition-transform duration-300 translate-y-24"
             v-for="(sound, key) in soundData"
             :soundItem="sound"
+            :data-index="key"
             :key="key"
             :soundItemKey="key"
         />
-    </div>
+    </StaggeredFade>
 </template>
 
 <script>
 import AudioItem from "./SoundItem";
+import StaggeredFade from "@/Components/Animations/StaggeredFade";
 
 //TODO: fix count plays function
 export default {
@@ -23,6 +30,7 @@ export default {
 
     components: {
         AudioItem,
+        StaggeredFade,
     },
     methods: {
         playedTrack(value) {
@@ -30,6 +38,29 @@ export default {
                 this.played.push(value);
             }
         },
+        getDelay(key) {
+            return "animation-delay:" + key * 1000;
+        },
     },
 };
 </script>
+
+<style scoped>
+/* we will explain what these classes do next! */
+@keyframes fadeIn {
+    0% {
+        opacity: 0;
+        transform: translateY(20px);
+    }
+    100% {
+        opacity: 1;
+        transform: translateY(0px);
+    }
+}
+
+.fadeIn {
+    -webkit-animation-name: fadeIn;
+    animation-name: fadeIn;
+    animation-duration: 1s;
+}
+</style>
