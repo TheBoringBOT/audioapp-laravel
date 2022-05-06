4378addb34530ae4f4ef25ec46fc0df0c58346ca
TheBoringBot <richardsgareth@icloud.com>
2022-05-06 23:01:43 +0200

    style: improved hero layout and height

--- resources/js/Pages/Frontend/Author.vue
+++ resources/js/Pages/Frontend/Author.vue
@@ -6,44 +6,50 @@
 
     <GuestAndUserFrontendLayout>
         <!--background for hero-->
-        <div
-            class="z-1 w-full h-full absolute left-0 top-0 bg-author-bg max-h-[55vh] min-h-[640px]"
-        ></div>
-        <ContentContainer>
-            <div
-                class="max-h-[55vh] min-h-[500px] flex flex-col pt-10 z-2 relative"
-            >
-                <!-- image -->
-                <div class="flex items-center justify-center relative">
-                    <img
-                        :src="author.avatar"
-                        alt=""
-                        class="rounded-full flex items-center justify-center w-48 h-48 mb-12 relative z-[1]"
-                    />
-                    <!--blur image-->
-                    <img
-                        :src="author.avatar"
-                        alt=""
-                        class="rounded-full w-48 h-48 blur-xl absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/4 opacity-50"
-                    />
-                </div>
-                <!-- text-->
-                <div
-                    class="flex flex-col items-center justify-center space-x-5"
-                >
-                    <span class="text-brand-clr font-medium">Author</span>
-                    <h1 class="font-semibold text-white text-5xl">
-                        {{ author.name }}
-                    </h1>
-                    <p
-                        class="font-medium text-secondary-clr text-xl mt-5 max-w-[600px] text-center"
-                    >
-                        {{ author.description }}
-                    </p>
-                </div>
-            </div>
 
-            <div v-if="soundData.length" class="my-16">
+        <div class="flex flex-col z-2 relative bg-author-bg">
+            <ContentContainer>
+                <div class="py-10">
+                    <!-- image -->
+                    <div class="flex items-center justify-center relative">
+                        <div
+                            class="rounded-full bg-secondary-bg w-32 h-32 md:w-48 md:h-48 block mb-2 md:mb-12 overflow-visible relative"
+                        >
+                            <img
+                                :src="author.avatar"
+                                alt=""
+                                class="rounded-full flex items-center justify-center w-full h-full relative z-[1]"
+                            />
+                            <!--blur image-->
+                            <img
+                                :src="author.avatar"
+                                alt=""
+                                class="rounded-full w-full h-full blur-xl absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/4 opacity-50"
+                            />
+                        </div>
+                    </div>
+                    <!-- text-->
+                    <div
+                        class="flex flex-col items-center justify-center space-x-5"
+                    >
+                        <span class="text-brand-clr font-medium">Author</span>
+                        <h1
+                            class="font-semibold text-white text-4xl md:text-5xl"
+                        >
+                            {{ author.name }}
+                        </h1>
+                        <p
+                            class="font-medium text-secondary-clr text-xl mt-2 md:mt-5 max-w-[600px] text-center"
+                        >
+                            {{ author.description }}
+                        </p>
+                    </div>
+                </div>
+            </ContentContainer>
+        </div>
+
+        <ContentContainer class="pt-5">
+            <div v-if="soundData.length" class="mb-16">
                 <SoundGrid :soundData="soundData" />
             </div>
         </ContentContainer>
