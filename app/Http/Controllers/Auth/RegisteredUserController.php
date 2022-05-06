1f3dcaae4813270b23dab1b2053a2cba79b62ab2
TheBoringBot <richardsgareth@icloud.com>
2022-05-06 22:09:51 +0200

    feat: upload configuration for user avatar and description

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -11,45 +11,83 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
+use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Intervention\Image\Facades\Image;
 
-class RegisteredUserController extends Controller
-{
-    /**
-     * Display the registration view.
-     *
-     * @return \Inertia\Response
-     */
-    public function create()
-    {
-        return Inertia::render('Auth/Register');
-    }
 
-    /**
-     * Handle an incoming registration request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+class RegisteredUserController extends Controller {
+	/**
+	 * Display the registration view.
+	 *
+	 * @return \Inertia\Response
+	 */
+	public function create() {
+		return Inertia::render( 'Auth/Register' );
+	}
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+	/**
+	 * Handle an incoming registration request.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 *
+	 * @return \Illuminate\Http\RedirectResponse
+	 *
+	 * @throws \Illuminate\Validation\ValidationException
+	 */
+	public function store( Request $request ) {
 
-        event(new Registered($user));
 
-        Auth::login($user);
+		$image_path = null;
 
-        return redirect(RouteServiceProvider::HOME);
-    }
+		$request->validate( [
+			'name'        => 'required|string|max:255',
+			'email'       => 'required|string|email|max:255|unique:users',
+			'description' => 'required|string|min:25|max:300',
+			'avatar'      => 'mimes:jpeg,jpg,png,gif|required|max:10000',
+			'password'    => [ 'required', 'confirmed', Rules\Password::defaults() ],
+		] );
+
+
+		if ( $request->avatar ) {
+			// Create slug for assets using the title from request
+
+			// Rename the Image file and save in the folder
+			$image      = $request->file( 'avatar' );
+			$image_name = uniqid() . '_' . $request->name . '.jpg';
+
+			$imgFile = Image::make( $image->getRealPath() );
+
+
+			//  (default uploaded image)
+
+			$imgFile->resize( 300, null, function ( $constraint ) {
+				$constraint->aspectRatio();
+			} )->save( 'images/profile/' . 'large_' . $image_name );
+
+			// create 1x image
+			$imgFile->resize( 150, null, function ( $constraint ) {
+				$constraint->aspectRatio();
+			} )->save( 'images/profile/' . 'small_' . $image_name );
+
+
+			// Image path for database
+			$image_path = 'images/profile/' . 'large_' . $image_name;
+
+		}
+
+
+		$user = User::create( [
+			'name'        => $request->name,
+			'email'       => $request->email,
+			'description' => $request->description,
+			'avatar'      => '/' . $image_path,
+			'password'    => Hash::make( $request->password ),
+		] );
+
+		event( new Registered( $user ) );
+
+		Auth::login( $user );
+
+		return redirect( RouteServiceProvider::HOME );
+	}
 }
