01f56dc3e2b507cdca53461efd0825dd25eba462
TheBoringBot <richardsgareth@icloud.com>
2022-05-07 19:09:39 +0200

    feat: page seo improved

--- resources/js/Pages/Frontend/SoundItem.vue
+++ resources/js/Pages/Frontend/SoundItem.vue
@@ -1,7 +1,7 @@
 <template>
     <Head>
         <title>{{ seo.title }}</title>
-        <meta name="description" :content="seo.description" />
+        <meta name="description" :content="seo.description"/>
     </Head>
 
     <GuestAndUserFrontendLayout class="text-primary-clr">
@@ -10,35 +10,35 @@
                 <ol class="list-reset flex">
                     <li>
                         <Link
-                            href="/sounds"
-                            class="font-semibold text-brand-clr hover:text-brand-clr-hover transition duration-150 ease-in-out"
-                            >Sounds</Link
-                        >
+                                href="/sounds"
+                                class="font-semibold text-brand-clr hover:text-brand-clr-hover transition duration-150 ease-in-out"
+                        >Sounds</Link
+                            >
                     </li>
                     <li>
                         <span class="font-semibold text-secondary-clr mx-2"
-                            >/</span
+                        >/</span
                         >
                     </li>
                     <li>
                         <Link
-                            :href="/sound/ + soundData.id"
-                            class="text-secondary-clr pointer-events-none"
-                            >Sound - {{ soundData.name }}</Link
-                        >
+                                :href="/sound/ + soundData.id"
+                                class="text-secondary-clr pointer-events-none"
+                        >Sound - {{ soundData.name }}</Link
+                            >
                     </li>
                 </ol>
             </nav>
             <div
-                class="pt-3 grid gap-0 md:gap-5 grid-cols-1 md:grid-cols-6 xl:grid-cols-5"
+                    class="pt-3 grid gap-0 md:gap-5 grid-cols-1 md:grid-cols-6 xl:grid-cols-5"
             >
                 <div class="col-span-4">
                     <!-- soundData item -->
                     <SoundItem
-                        :soundItem="soundData"
-                        :key="soundData.id"
-                        :soundItemKey="soundData.id"
-                        :currentUserData="currentUserData"
+                            :soundItem="soundData"
+                            :key="soundData.id"
+                            :soundItemKey="soundData.id"
+                            :currentUserData="currentUserData"
                     />
                     <!-- Description -->
                     <div class="py-5">
@@ -48,28 +48,28 @@
                     <!-- Tags -->
                     <div class="mt-4">
                         <span class="mr-3 text-primary-clr font-semibold"
-                            >Tags:</span
+                        >Tags:</span
                         >
                         <Link
-                            :href="`/search` + '?' + `keyword=${tag}`"
-                            class="text-primary-clr font-semibold bg-secondary-bg hover:bg-secondary-bg-hover transition duration-150 ease-in-out px-3 py-2 mr-3 rounded"
-                            v-for="tag in tags"
-                            :key="tag.id"
-                            >{{ tag }}</Link
-                        >
+                                :href="`/search` + '?' + `keyword=${tag}`"
+                                class="text-primary-clr font-semibold bg-secondary-bg hover:bg-secondary-bg-hover transition duration-150 ease-in-out px-3 py-2 mr-3 rounded"
+                                v-for="tag in tags"
+                                :key="tag.id"
+                        >{{ tag }}</Link
+                            >
                     </div>
                 </div>
                 <div
-                    class="mt-12 md:mt-0 col-span-2 xl:col-span-1 w-full flex grow"
+                        class="mt-12 md:mt-0 col-span-2 xl:col-span-1 w-full flex grow"
                 >
                     <div
-                        class="border border-secondary-bg xl:text-lg flex grow flex-col p-5 bg-secondary-bg justify-between"
+                            class="border border-secondary-bg xl:text-lg flex grow flex-col p-5 bg-secondary-bg justify-between"
                     >
                         <div>
                             <div class="flex justify-between">
                                 <span>Bit Depth:</span>
                                 <span class="text-secondary-clr"
-                                    >{{ soundData.bit_depth }} bit</span
+                                >{{ soundData.bit_depth }} bit</span
                                 >
                             </div>
                             <div class="flex justify-between">
@@ -106,13 +106,13 @@
                         </div>
                         <div class="mt-4 lg:mt-0">
                             <button
-                                @click="
+                                    @click="
                                     download(
                                         soundData.id,
                                         $page.props.auth.user
                                     )
                                 "
-                                class="text-base w-full block font-semibold text-primary-bg px-6 py-2 bg-brand-clr hover:bg-brand-clr-hover hover:border-0 rounded"
+                                    class="text-base w-full block font-semibold text-primary-bg px-6 py-2 bg-brand-clr hover:bg-brand-clr-hover hover:border-0 rounded"
                             >
                                 Download Sound
                             </button>
@@ -127,10 +127,10 @@
 <script>
 import GuestAndUserFrontendLayout from "@/Components/Layouts/GuestAndUserFrontendLayout.vue";
 import ContentContainer from "@/Components/Layouts/ContentContainer";
-import { Head, Link } from "@inertiajs/inertia-vue3";
-import { useToast } from "vue-toastification";
+import {Head, Link} from "@inertiajs/inertia-vue3";
+import {useToast} from "vue-toastification";
 import SoundItem from "@/Components/Grids/SoundItem";
-import { formatSize } from "@/Helpers";
+import {formatSize} from "@/Helpers";
 
 export default {
     components: {
@@ -144,7 +144,7 @@ export default {
     data(props) {
         return {
             seo: {
-                title: "sound" + "-" + props.soundData.name,
+                title: "sound" + " | " + props.soundData.name,
                 description: props.soundData.description,
             },
         };
@@ -152,7 +152,7 @@ export default {
 
     setup() {
         const toast = useToast();
-        return { toast };
+        return {toast};
     },
 
     methods: {
