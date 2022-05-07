bf402982a62d183fce91064ac7057782443655ac
TheBoringBot <richardsgareth@icloud.com>
2022-05-07 20:12:50 +0200

    style: improved spacing on tags

--- resources/js/Components/PopularTags.vue
+++ resources/js/Components/PopularTags.vue
@@ -3,7 +3,9 @@
         <span class="pb-4 lg:pb-0 lg:mr-3 text-primary-clr font-semibold"
             >Top Tags:</span
         >
-        <div class="flex-row">
+        <div
+            class="flex whitespace-wrap overflow-hidden space-x-2 md:space-x-5"
+        >
             <Link
                 v-for="tag in popularTags"
                 :key="tag.id"
@@ -15,8 +17,11 @@
                         ? 'bg-secondary-bg-hover text-brand-clr'
                         : 'text-primary-clr'
                 "
-                class="cursor-pointer font-semibold bg-secondary-bg hover:bg-secondary-bg-hover transition duration-150 ease-in-out px-3 py-2 mr-3 rounded"
-                ><span class="capitalize inline-block">{{ tag }}</span></Link
+                class="cursor-pointer font-semibold bg-secondary-bg hover:bg-secondary-bg-hover transition duration-150 ease-in-out px-3 py-2 rounded"
+                ><span
+                    class="capitalize whitespace-nowrap overflow-hidden text-ellipsis"
+                    >{{ tag }}</span
+                ></Link
             >
         </div>
     </div>
